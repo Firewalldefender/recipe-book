c7ac88249790c5d8eda4c280ea8c5c5c180ab986
Firewalldefender <marlinbrubacher04@gmail.com>
2026-09-10 17:25:35 +0200

    createRecipeFunction

--- src/app/createrecipe/page.tsx
+++ src/app/createrecipe/page.tsx
@@ -1,6 +1,45 @@
 "use client";
+
+import { error } from "console";
+import { useActionState } from "react";
+import { success } from "zod";
+import { createRecipe } from "../../../utils/recipe";
+
+//1:00:00 in Renkes video
+
 export const CreateRecipe = () => {
-  return <div>Erstelle dein Rezept</div>;
+  const [state, formAction, isPending] = useActionState(createRecipe, {
+    error: null,
+    success: false,
+  });
+
+  return (
+    <div>
+      <h2>Erstelle dein Rezept</h2>
+      <form action={formAction}>
+        <input name="title" placeholder="Rezept Name" required />
+        <input name="category" placeholder="Was wird es..." required />
+        <input
+          name="duration"
+          type="number"
+          placeholder="Wie lange dauert es?"
+          required
+        />
+        <input
+          name="servings"
+          type="number"
+          placeholder="Für wie viele reicht es?"
+          required
+        />
+        {state.error && <p className="text-red-500">{state.error}</p>}
+        {state.success && <p className="text-green-500">{state.message}</p>}
+        <button disabled={isPending}>
+          {" "}
+          {isPending ? "Erstellen..." : "Rezept erstellen"}
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default CreateRecipe;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import RecipeCard from "../../components/recipeCard";
 import { getRecipes } from "../../utils/recipe";
 
 export default async function Home() {
@@ -9,19 +9,7 @@ export default async function Home() {
       <div>
         <h2>Omi´s Rezepte</h2>
         {recipes.map((recipe) => (
-          <div key={recipe.id} className="p-3 space-y-2 border">
-            <h2>{recipe.title}</h2>
-            <Image
-              src={recipe.image}
-              alt={recipe.title}
-              width={200}
-              height={200}
-              loading="eager"
-            />
-            <p> Category: {recipe.category}</p>
-            <p>Time: {recipe.duration}</p>
-            <p>Servings: {recipe.servings}</p>
-          </div>
+          <RecipeCard key={recipe.id} recipe={recipe} />
         ))}
       </div>
     </main>
