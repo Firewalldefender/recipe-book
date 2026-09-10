9f3f7cf1c48e07c377c15c3f4184cb778c30f4e2
Firewalldefender <marlinbrubacher04@gmail.com>
2026-09-10 13:57:24 +0200

    homePage-with-fetchedData

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
-export default function Home() {
+import Image from "next/image";
+import { getRecipes } from "../../utils/recipe";
+
+export default async function Home() {
+  const recipes = await getRecipes();
+
   return (
-    <div>
-      <h2>Omi´s Rezepte</h2>
-    </div>
+    <main>
+      <div>
+        <h2>Omi´s Rezepte</h2>
+        {recipes.map((recipe) => (
+          <div key={recipe.id} className="p-3 space-y-2 border">
+            <h2>{recipe.title}</h2>
+            <Image
+              src={recipe.image}
+              alt={recipe.title}
+              width={200}
+              height={200}
+              loading="eager"
+            />
+            <p> Category: {recipe.category}</p>
+            <p>Time: {recipe.duration}</p>
+            <p>Servings: {recipe.servings}</p>
+          </div>
+        ))}
+      </div>
+    </main>
   );
 }
